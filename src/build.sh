ff1be92b32c5210a87b4443ca17e3fd53d9526ca
Benjamin Hollis <benjamin.hollis@snapchat.com>
2021-05-26 14:24:16 -0700

    Add a GitHub workflow to build & test

--- src/build.sh
+++ src/build.sh
@@ -4,16 +4,16 @@ set -eu
 # Locate the script file.  Cross symlinks if necessary.
 loc="$0"
 while [ -h "$loc" ]; do
-    ls=`ls -ld "$loc"`
-    link=`expr "$ls" : '.*-> \(.*\)$'`
+    ls=$(ls -ld "$loc")
+    link=$(expr "$ls" : '.*-> \(.*\)$')
     if expr "$link" : '/.*' > /dev/null; then
         loc="$link"  # Absolute link
     else
-        loc="`dirname "$loc"`/$link"  # Relative link
+        loc="$(dirname "$loc")/$link"  # Relative link
     fi
 done
-base_dir=$(cd "`dirname "$loc"`" && pwd)
+base_dir=$(cd "$(dirname "$loc")" && pwd)
 
-pushd $base_dir
+pushd "$base_dir"
 ${BAZEL_EXECUTABLE:-bazel} build //src:djinni
 popd
