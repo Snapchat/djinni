d90640f6138bb2402ddacf5e94e4c551bd80a32d
Li Feng <li.feng@snapchat.com>
2021-08-16 14:57:30 +1000

    logging

--- examples/wasm/world_djinni.js
+++ examples/wasm/world_djinni.js
@@ -1,6 +1,6 @@
 // ---- Library code
 Module.cppProxyFinalizerRegistry = new FinalizationRegistry(nativeRef => {
-    console.log("finalizing " + nativeRef);
+    console.log("finalizing cpp object");
     nativeRef.nativeDestroy();
     nativeRef.delete();
 });
@@ -63,18 +63,21 @@ function same_c(a, b, desc) {
 }
 
 let main = function() {
+    console.log("-------- 1")
     {
         var i = Module.MyInterface.instance();
         var j = Module.MyInterface.instance();
         same(i, j, "same c++ object passed to js twice");
     }
 
+    console.log("-------- 2")
     {
         var i = Module.MyInterface.create();
         var j = i;
         same_c(i, j, "cpp object to js and back");
     }
 
+    console.log("-------- 3")
     {
         var i = new MyInterfaceJS();
         var j = new MyInterfaceJS();
@@ -82,28 +85,33 @@ let main = function() {
         same_c(i, i, "same js object passed to cpp twice");
     }
 
+    console.log("-------- 4")
     {
         var i = new MyInterfaceJS();
         var j = Module.MyInterface.pass(i);
         same(i, j, "js object to cpp and back");
     }
 
+    console.log("-------- 5")
     {
         var i = Module.MyInterface.instance();
         console.log("got string: " + i.testStr("hello"));
     }
 
+    console.log("-------- 6")
     {
         var i = Module.MyInterface.instance();
         i.foo(32);
     }
 
+    console.log("-------- 7")
     {
         var i = Module.MyInterface.instance();
         var v = i.testBin(new Uint8Array([1,2,3]));
         console.log(v);
     }
 
+    console.log("-------- 8")
     {
         var d = new Date()
         console.log(d);
@@ -112,9 +120,12 @@ let main = function() {
         console.log(d);
     }
 
+    console.log("-------- 9")
     {
         var i = Module.MyInterface.instance();
         var r = { _x: 100, _y: "hello" };
         console.log(i.testRecord(r));
     }
+
+    console.log("-------- 10")
 }
