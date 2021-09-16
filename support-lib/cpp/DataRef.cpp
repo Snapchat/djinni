793f1bade433c6bf84705b57360c53f7d9608b4b
Li Feng <li.feng@snapchat.com>
2021-09-16 11:15:22 +1000

    cast

--- support-lib/cpp/DataRef.cpp
+++ support-lib/cpp/DataRef.cpp
@@ -194,8 +194,8 @@ static void DataRefHelper_nativeDestroy(JNIEnv* /*unused*/, jclass /*unused*/, j
 }
 
 static const JNINativeMethod kNativeMethods[] = {{
-    (char*) "nativeDestroy",
-    (char*) "(J)V",
+    const_cast<char*>("nativeDestroy"),
+    const_cast<char*>("(J)V"),
     reinterpret_cast<void*>(&DataRefHelper_nativeDestroy),
 }};
 
