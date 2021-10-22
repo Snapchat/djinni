fd7a79558da5ff70023c3fda99d6c46f3ddfa2c1
Li Feng <li.feng@snapchat.com>
2021-10-22 12:17:02 +1100

    use relative path in yaml module path

--- src/source/YamlGenerator.scala
+++ src/source/YamlGenerator.scala
@@ -189,7 +189,7 @@ class YamlGenerator(spec: Spec) extends Generator(spec) {
 
   private def ts(td: TypeDecl) = Map[String, Any](
     "typename" -> tsMarshal.toTsType(mexpr(td)),
-    "module" -> QuotedString(spec.tsModule)
+    "module" -> QuotedString("./" + spec.tsModule)
   )
 
   // TODO: there has to be a way to do all this without the MExpr/Meta conversions?
