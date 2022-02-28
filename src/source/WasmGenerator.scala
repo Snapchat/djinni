c0feb80f9e339722f70370194346a00a76fc4c2a
Li Feng <li.feng@snapchat.com>
2022-02-28 13:38:40 +1100

    comment

--- src/source/WasmGenerator.scala
+++ src/source/WasmGenerator.scala
@@ -376,6 +376,9 @@ class WasmGenerator(spec: Spec) extends Generator(spec) {
             }
             w.w("catch(const std::exception& e)").braced {
               w.wl("djinni::djinni_throw_native_exception(e.what());");
+              // The throw line is just to let the C++ compiler know that this
+              // branch won't return a value. Execution will never reach this
+              // line as the previous line already throws in JS code.
               w.wl("throw;");
             }
           }
