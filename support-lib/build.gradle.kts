83fd34cdd8b05aee9b434c02b44307af71631f27
Maksim Savoskin <maksim.savoskin@bandlab.com>
2024-06-28 11:00:40 +0800

    added support for publishing support-lib to maven repository

---
+++ support-lib/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+}
+
+// We have BUILD file from basel
+getLayout().buildDirectory.set(file("gradle_build"))
+
+sourceSets {
+    main {
+        java {
+            srcDirs("java")
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.bandlab"
+            artifactId = "djinni-support-lib"
+            version = project.providers.gradleProperty("version").orNull
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven(project.providers.gradleProperty("artifactoryPublishUrl")) {
+            name = "artifactory"
+            credentials(PasswordCredentials::class.java)
+        }
+    }
+}
